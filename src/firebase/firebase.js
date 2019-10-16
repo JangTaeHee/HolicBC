3f6ebe457e5461ecce3d9b511262ea345deb90cb
JangTaeHee <31396064+JangTaeHee@users.noreply.github.com>
2019-10-17 00:55:58 +0900

    admin template commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBmyGbFQMmLcHtf9rNAmUjObo_816mKPUU",
+  authDomain: "holicbc-986b9.firebaseapp.com",
+  databaseURL: "https://holicbc-986b9.firebaseio.com",
+  projectId: "holicbc-986b9",
+  storageBucket: "holicbc-986b9.appspot.com",
+  messagingSenderId: "101286249031",
+  appId: "1:101286249031:web:2a9d2984c006a8a1ac6762",
+  measurementId: "G-PH1VC328MH"
+};
